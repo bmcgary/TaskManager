1cdaf7b887e0c737b08053da612ade253b7b46e4
Blaine McGary <bmcgary@byu.edu>
2015-01-03 23:11:33 -0700

    added tracking features to server.js

--- server.js
+++ server.js
@@ -12,16 +12,20 @@ var port = 1337;
 
 var router = express.Router();
 
-router.post('/register',function(req,res){routesHandled.addAUser(req,res)});
-router.post('/login',function(req,res){routesHandled.login(req,res)});
-router.post('/updateList',function(req,res){routesHandled.updateList(req,res)});
-router.post('/getAllTasks',function(req,res){routesHandled.getAllTasks(req,res)});
-router.post('/addTask',function(req,res){routesHandled.addTask(req,res)});
-router.post('/deleteTask',function(req,res){routesHandled.deleteTask(req,res)});
-router.get('/test',function(req,res){res.json({message:'default'});});
+router.post('/register',function(req,res){output(req,"register");routesHandled.addAUser(req,res)});
+router.post('/login',function(req,res){output(req,"login");routesHandled.login(req,res)});
+router.post('/updateList',function(req,res){output(req,"updateList");routesHandled.updateList(req,res)});
+router.post('/getAllTasks',function(req,res){output(req,"getAllTasks");routesHandled.getAllTasks(req,res)});
+router.post('/addTask',function(req,res){output(req,"addTask");routesHandled.addTask(req,res)});
+router.post('/deleteTask',function(req,res){output(req,"deleteTask");routesHandled.deleteTask(req,res)});
+router.get('/test',function(req,res){output(req,"test");res.json({message:'default'});});
 
 app.use('/',router);
 
 app.listen(port);
 console.log("Server started");
 
+function output(req,origin)
+{
+	console.log("Time:" + Date() + " IP:" + req.connection.remoteAddress + " At:" + origin);
+}
